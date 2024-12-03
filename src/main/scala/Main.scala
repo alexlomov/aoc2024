a0b53b7135f88feaac780645124b3bd83f095b6b
Alexander Lomov <alexander.lomov@me.com>
2024-12-03 17:18:30 +0100

    Day1, task2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,26 @@
 @main def main(): Unit =
   val t1Input = day1.loadDay1("src/main/resources/d1t1.txt")
-  val t1 = day1.task1.tupled(t1Input)
+
+  // Day1, task1
+  val t1 = day1.task1Distance.tupled(t1Input)
   println(t1)
 
+  //Day1, task2
+  val t2 = day1.task2Similarity.tupled(t1Input)
+  print(t2)
+
 object day1:
-  def task1(lh: List[Int], rh: List[Int]): Int =
+  def task1Distance(lh: List[Int], rh: List[Int]): Int =
     lh.sorted.zip(rh.sorted).map:
       (l, r) => l - r
     .map:
       (v) => if v < 0 then v * -1 else v
     .fold(0):
       _ + _
+
+  def task2Similarity(lh: List[Int], rh: List[Int]): Int =
+    lh.fold(0):
+      (acc, v) => (rh.filter(_ == v).length * v) + acc
 
   def loadDay1(fileName: String): (List[Int], List[Int]) =
     scala.io.Source.fromFile(fileName).getLines
